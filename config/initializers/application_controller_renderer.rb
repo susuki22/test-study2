38a79c32aba1947e9d25badd9c8b03e6e2d2999a
susuki22 <shoya.s.0427@gmail.com>
2022-01-26 09:56:25 +0000

    機能追加

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '9fb9a21a3086fea904c90d210a4ed4849965f1a96e72833daf990834684ab6a075c2ab59e247b7df332eeb0522dd264d851fb86c81d3bcc587b2de77a6181b9f'
