6e536a21611f98e62414b6db1aa04f2c42e63628
susuki22 <shoya.s.0427@gmail.com>
2022-01-30 08:14:44 +0000

    コメント機能追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_26_082228) do
+ActiveRecord::Schema.define(version: 2022_01_30_075632) do
+
+  create_table "post_comments", force: :cascade do |t|
+    t.text "comment"
+    t.integer "user_id"
+    t.integer "post_image_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "post_images", force: :cascade do |t|
     t.text "shop_name"
